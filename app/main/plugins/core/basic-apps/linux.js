b807b8c725d38d2df9ed02fb9b1800cdd63eec45
Alexandr Subbotin <kelionweb@gmail.com>
2017-02-01 20:40:32 +0100

    Fix opening apps with %u or other arguments

--- app/main/plugins/core/basic-apps/linux.js
+++ app/main/plugins/core/basic-apps/linux.js
@@ -29,7 +29,12 @@ export const DIRECTORIES = uniq([
 
 export const EXTENSIONS = ['desktop']
 
-export const openApp = (app) => shellCommand(app.exec)
+export const openApp = ({ exec }) => {
+  // Replace %u and other % arguments in exec script
+  // https://github.com/KELiON/cerebro/pull/62#issuecomment-276511320
+  const cmd = exec.replace(/%./g, '')
+  shellCommand(cmd)
+}
 
 const parseDesktopFile = (filePath, mapping) => {
   const content = fs.readFileSync(filePath, 'utf-8')
